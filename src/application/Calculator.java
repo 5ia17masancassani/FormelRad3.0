c64a5e7f51a8e072b00a596801c010ff4c9ceac8
Chapman Nils, GHR-OSA-NEX-15 <Nils.Chapman@swisscom.com>
2018-11-25 19:14:40 +0100

    Grund Strucktur des Projekts erstellt.

---
+++ src/application/Calculator.java
@@ -0,0 +1,57 @@
+package application;
+
+/**
+ * Berechnet das Formelrad
+ * @author Peter Rutschmann
+ * @version 13.09.2018
+ */
+public class Calculator {
+	private double leistung;
+	private double spannung;
+	private double strom;
+	private double widerstand;
+	
+	public Calculator(double leistung, double spannung, double strom, double widerstand) {
+		super();
+		this.leistung = leistung;
+		this.spannung = spannung;
+		this.strom = strom;
+		this.widerstand = widerstand;
+	}
+	
+	public double getLeistung() {
+		return leistung;
+	}
+	
+	public double getSpannung() {
+		return spannung;
+	}
+
+	public double getStrom() {
+		return strom;
+	}
+
+	public double getWiderstand() {
+		return widerstand;
+	}
+
+	@Override
+	public String toString() {
+		return "Calculator [leistung=" + leistung + 
+				", spannung=" + spannung + 
+				", strom=" + strom + 
+				", widerstand="	+ widerstand + "]";
+	}
+
+	public void calculate() {
+	}
+
+	//Strom aus 
+	public double iAusPundR(double p,double r){
+		return Math.sqrt(p/r);
+	}
+	
+
+
+	
+}
